a6527fabeb94335cf9800a2f71154f71673b3f26
Ryan Vilim <ryan.vilim@gmail.com>
2016-10-19 23:15:22 -0400

    A bit of code cleanup

--- scrapr/scrape.py
+++ scrapr/scrape.py
@@ -6,7 +6,6 @@ import json
 from boto.s3.connection import S3Connection
 import time
 import sys
-from Queue import Queue
 
 class Scrape():
 
@@ -17,10 +16,13 @@ class Scrape():
         s3_client = S3Connection(aws_access_key, aws_secret_key)
         self.bucket = s3_client.get_bucket(s3_bucket)
 
+        # List the files in our bucket, find jsons and remove them, leaving the id, then setify it. That way we don't
+        # download the same file twice
         self.scraped_ids = set([filename.key[:-5] for filename in self.bucket.get_all_keys() if filename.key[-5:]=='.json'])
 
         self.tag = tag
         self.refresh_period = refresh_period
+        self.scraping = False
 
     def input_thread(self):
         raw_input("Press any key to stop")
@@ -30,10 +32,8 @@ class Scrape():
         logging.info("Starting scrape.")
         self.scraping = True
 
-
         i = 1
 
-        q=Queue()
         interrupt_thread = Thread(target=self.input_thread)
         interrupt_thread.start()
 
@@ -44,33 +44,32 @@ class Scrape():
             sys.stdout.flush()
             sys.stdout.write("Scrape #" + str(i) + " " + loader[i % 4] + "    Press any key to stop")
 
-            t=Thread(target=self.get_posts)
+            # Spin off a thread to get the posts. This doesn't technically need to be in a thread, but I wanted
+            # to future proof it in case I want to get multiple tags concurrently.
+            t = Thread(target=self.post_thread)
             t.start()
             t.join()
             time.sleep(self.refresh_period)
-            i+=1
+            i += 1
 
     def stop(self):
         sys.stdout.flush()
         self.scraping = False
 
-    def get_posts(self):
-        postthread=Thread(target=self.post_thread)
-        postthread.start()
-        postthread.join()
-
     def post_thread(self):
         response = self.tumblr_client.tagged(self.tag)
         content_list = self.parse_content_urls(response)
 
-        threads=[]
+        image_threads = []
 
+        # Loop through the posts that got returned, then spin off threads to download the images associated with them.
         for content in content_list:
-            threads.append(Thread(target=self.upload_content, args=(content,)))
-            threads[-1].start()
+            image_threads.append(Thread(target=self.upload_content, args=(content,)))
+            image_threads[-1].start()
 
-        for thread in threads:
-            thread.join()
+        # Wait for all the images to download before we close the post thread.
+        for image_thread in image_threads:
+            image_thread.join()
 
     def upload_content(self, content):
         id = str(content[0])
@@ -83,6 +82,7 @@ class Scrape():
             img_url = content[2]
             img_filename = img_url.split('/')[-1]
 
+            # We put the image in a directory with the post id in the directory name
             image_key = self.bucket.new_key(id+'/'+img_filename)
             r = requests.get(img_url)
 
@@ -100,6 +100,8 @@ class Scrape():
     def parse_content_urls(self, posts):
         content = []
 
+        # If the post doesn't have one and only one photo in it, we will still save the json (for later), but won't
+        # download any photos.
         for post in posts:
             id = post['id']
 
