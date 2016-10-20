8ee66cecd5769f4c510ec3c7135c855f8be8ef44
Ryan Vilim <ryan.vilim@gmail.com>
2016-10-19 23:05:24 -0400

    Got it up to the point where it now has basic functionality
    Squashed commit of the following:
    
    commit 826d09573f34a55d26f8aaa90c0b367f2f4c1e7b
    Author: Ryan Vilim <ryan.vilim@gmail.com>
    Date:   Wed Oct 19 23:04:51 2016 -0400
    
        Fixed some application.ini issues
    
    commit 59ac5a0378d097b6038071c609a8528b84c522a0
    Author: Ryan Vilim <ryan.vilim@gmail.com>
    Date:   Wed Oct 19 22:50:25 2016 -0400
    
        Made the stopping interface a bit better
    
    commit 64c59235a20ffb39ae0349224ffb5e37005eb2e8
    Author: Ryan Vilim <ryan.vilim@gmail.com>
    Date:   Wed Oct 19 22:07:00 2016 -0400
    
        Added code to skip re-downloading things we've already scraped.
    
    commit 55209e7eb0508401ff35f3ea99d10c7f752b0022
    Author: Ryan Vilim <ryan.vilim@gmail.com>
    Date:   Wed Oct 19 21:52:22 2016 -0400
    
        Working version, probably have to tweak the rate a bit to not get banned.

--- scrape.py
+++ scrape.py
@@ -1,8 +1,112 @@
 import pytumblr
+from threading import Thread
+import requests
+import logging
+import json
+from boto.s3.connection import S3Connection
+import time
+import sys
+from Queue import Queue
 
 class Scrape():
 
-    def __init__(self, api_key, tags):
-        self.api_key = api_key
-        self.client = pytumblr.TumblrRestClient(api_key)
-        self.tags = tags
+    def __init__(self, aws_access_key, aws_secret_key, s3_bucket, tumblr_api_key, tag, refresh_period=1.0):
+
+        self.tumblr_client = pytumblr.TumblrRestClient(tumblr_api_key)
+
+        s3_client = S3Connection(aws_access_key, aws_secret_key)
+        self.bucket = s3_client.get_bucket(s3_bucket)
+
+        self.scraped_ids = set([filename.key[:-5] for filename in self.bucket.get_all_keys() if filename.key[-5:]=='.json'])
+
+        self.tag = tag
+        self.refresh_period = refresh_period
+
+    def input_thread(self):
+        raw_input("Press any key to stop")
+        self.scraping = False
+
+    def start(self):
+        logging.info("Starting scrape.")
+        self.scraping = True
+
+
+        i = 1
+
+        q=Queue()
+        interrupt_thread = Thread(target=self.input_thread)
+        interrupt_thread.start()
+
+        while self.scraping:
+            loader = {0: '|', 1: '/', 2: '-', 3: '\\'}
+
+            sys.stdout.write('\r')
+            sys.stdout.flush()
+            sys.stdout.write("Scrape #" + str(i) + " " + loader[i % 4] + "    Press any key to stop")
+
+            t=Thread(target=self.get_posts)
+            t.start()
+            t.join()
+            time.sleep(self.refresh_period)
+            i+=1
+
+    def stop(self):
+        sys.stdout.flush()
+        self.scraping = False
+
+    def get_posts(self):
+        postthread=Thread(target=self.post_thread)
+        postthread.start()
+        postthread.join()
+
+    def post_thread(self):
+        response = self.tumblr_client.tagged(self.tag)
+        content_list = self.parse_content_urls(response)
+
+        threads=[]
+
+        for content in content_list:
+            threads.append(Thread(target=self.upload_content, args=(content,)))
+            threads[-1].start()
+
+        for thread in threads:
+            thread.join()
+
+    def upload_content(self, content):
+        id = str(content[0])
+        post = content[1]
+
+        if id in self.scraped_ids:
+            return
+
+        if content[2] is not None:
+            img_url = content[2]
+            img_filename = img_url.split('/')[-1]
+
+            image_key = self.bucket.new_key(id+'/'+img_filename)
+            r = requests.get(img_url)
+
+            if r.status_code == 200:
+                json_key = self.bucket.new_key(id + '.json')
+                json_key.set_contents_from_string(json.dumps(post))
+
+                image_key.content_type = r.headers['content-type']
+                image_key.set_contents_from_string(r.content)
+
+        else:
+            json_key = self.bucket.new_key(id + '.json')
+            json_key.set_contents_from_string(json.dumps(post))
+
+    def parse_content_urls(self, posts):
+        content = []
+
+        for post in posts:
+            id = post['id']
+
+            if post['type'] == 'photo' and len(post['photos']) == 1:
+                img_url=post['photos'][0]['original_size']['url']
+                content.append((id, post, img_url))
+            else:
+                content.append((id, post, None))
+
+        return content
